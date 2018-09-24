cf8a2192de0a2e5e3dcc9d314424b22149a70881
Lucca Kaiser <luccakaiser@gmail.com>
2018-09-24 18:16:31 +0200

    Finish working and refactored palindrome method

--- lib/luccakaiser_palindrome.rb
+++ lib/luccakaiser_palindrome.rb
@@ -1,5 +1,16 @@
 require "luccakaiser_palindrome/version"
 
-module LuccakaiserPalindrome
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      self.scan(/[a-z]/i).join.downcase
+    end
 end
